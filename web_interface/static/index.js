2312a13fe4d34ffd50cbed2c501828ed7aade319
Stefan Aleksic <stefan@stefanaleksic.com>
2018-04-03 11:14:50 -0400

    Cleanup code and add comments to js.

--- web_interface/static/index.js
+++ web_interface/static/index.js
@@ -1,5 +1,4 @@
 $(function () {
-
     const entityMap = {
         '&': '&amp;',
         '<': '&lt;',
@@ -9,16 +8,19 @@ $(function () {
         '/': '&#x2F;',
         '`': '&#x60;',
         '=': '&#x3D;'
-      };
+    };
 
-    // The following code was borrowed from a Stackoverflow answer on escaping HTML to mitigate XSS
+    // The following code is from a Stackoverflow answer on escaping HTML to mitigate XSS
     // https://stackoverflow.com/questions/24816/escaping-html-strings-with-jquery
-    function escapeHtml (string) {
+    function escapeHtml(string) {
         return String(string).replace(/[&<>"'`=\/]/g, function (s) {
             return entityMap[s];
         });
     }
 
+    /**
+     * getInfectedHosts queries the API to get all the current infected hosts' MAC addresses.
+     */
     async function getInfectedHosts() {
         const ajaxCall = await $.ajax({
             url: "/get_infected_hosts",
@@ -27,11 +29,15 @@ $(function () {
         return JSON.parse(ajaxCall).map(escapeHtml);
     }
 
+    /**
+     * getPayloads queries the API to get all of the payloads a given MAC address sent.
+     * @param {string} macAddress MAC address one would like to get payloads from.
+     */
     async function getPayloads(macAddress) {
         const ajaxCall = await $.ajax({
             url: "/get_payloads?mac_address=" + macAddress,
             cache: false
-        })
+        });
         return JSON.parse(ajaxCall).map((payload) => {
             return {
                 name: escapeHtml(payload.name),
@@ -40,6 +46,11 @@ $(function () {
         });
     }
 
+    /**
+     * getPayload queries the API to get a specific payload
+     * @param {string} macAddress the infected host's MAC address to get payload from.
+     * @param {string} payloadId specific payload's identifier.
+     */
     async function getPayload(macAddress, payloadId) {
         const ajaxCall = await $.ajax({
             url: "/get_payload?mac_address=" + macAddress + "&payload_id=" + payloadId,
@@ -48,8 +59,15 @@ $(function () {
         return escapeHtml(ajaxCall);
     }
 
+    // This toggle main state variable is used to be able to perform complete cascading `display:none` even when some
+    // children nodes are already hidden.
     var toggledMainState = 0;
 
+    /**
+     * toggleVisibility switches the CSS styling of a JQuery element from display: block to display: none and vice versa
+     * @param {JQuery Element} someElement the element one would like to set visible or not visible
+     * @param {boolean} shouldUseMainState whether the visibility should be viewed from a macro level vs a micro level
+     */
     function toggleVisibility(someElement, shouldUseMainState) {
         if (shouldUseMainState === 1) {
             if (toggledMainState === 0) {
@@ -68,6 +86,10 @@ $(function () {
         }
     }
 
+    /**
+     * toggleChildrenVisibility toggles the current node's visibility and also all of its children's visibility.
+     * it is to be used within a JQuery context as it uses an implicit `$(this)`.
+     */
     function toggleChildrenVisibility() {
         toggleVisibility($(this));
         const children = $(this).children();
@@ -76,10 +98,14 @@ $(function () {
         });
     }
 
+    /**
+     * createHostLink creates the JQuery element that defines the view and implementation of an infected host's anchor tag link. 
+     * @param {string} host The MAC Address of an infected host.
+     */
     function createHostLink(host) {
         const hostLink = $("<a>");
 
-        hostLink.append(host)
+        hostLink.append(host);
         hostLink.attr("href", "#");
         hostLink.addClass("host_link");
 
@@ -87,27 +113,36 @@ $(function () {
             $(this).siblings().each(toggleChildrenVisibility);
             return false;
         });
-
         return hostLink;
     }
 
+    /**
+     * createHostDiv creates the JQuery element used for storing the hosts
+     */
     function createHostDiv() {
         const hostDiv = $("<div>");
         hostDiv.addClass("host_name");
         return hostDiv;
     }
 
+    /**
+     * createUserPayloadsDiv creates the JQuery element used for storing user payloads
+     */
     function createUserPayloadsDiv() {
         const userPayloadsDiv = $("<div>");
         userPayloadsDiv.addClass("user_payloads");
         return userPayloadsDiv;
     }
 
+    /**
+     * createUserPayloadsLink creates the JQuery element that defines the view and implementation of an payload's anchor tag link. 
+     * @param {string} payload the payload one would like to base the link around.
+     */
     function createUserPayloadsLink(payload) {
         const userPayloadsLink = $("<div>");
         userPayloadsLink.append("Name: ");
 
-        const userPayloadsLinkAnchor  = $("<a>");
+        const userPayloadsLinkAnchor = $("<a>");
         userPayloadsLinkAnchor.attr("href", "#");
         userPayloadsLinkAnchor.addClass("payload_name_link");
         userPayloadsLinkAnchor.append(payload['name']);
@@ -122,6 +157,9 @@ $(function () {
         return userPayloadsLink;
     }
 
+    /**
+     * createUI gets the infected hosts along with their payloads and renders the information.
+     */
     async function createUI() {
         const infectedHosts = await getInfectedHosts();
         infectedHosts.forEach(async (host) => {
@@ -136,11 +174,11 @@ $(function () {
             payloads.forEach(async (payloadObject) => {
                 const userPayloadsDiv = createUserPayloadsDiv();
                 const userPayloadsLink = createUserPayloadsLink(payloadObject);
-                
+
                 userPayloadsDiv.append(userPayloadsLink);
 
                 const payload = await getPayload(host, payloadObject['payload_id']);
-                
+
                 const payloadDataDiv = $("<pre class=\"payload\"></pre>");
                 payloadDataDiv.append(payload);
                 const containerDiv = $("<div class=\"container\"></div>");
@@ -150,9 +188,10 @@ $(function () {
                 hostDiv.append(containerDiv);
             });
         });
-    };
+    }
 
-    (async function() {
+    // Call the createUI function to render the UI.
+    (async function () {
         await createUI();
     })();
 });
